5bc6d6d93e07aa1bfaba18787ad060a99e590206
sua <dssua0714@naver.com>
2025-04-23 11:49:08 +0900

    메인 레이아웃 설정

---
+++ react-vite-psa-ecommerce/src/router/Router.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom"
+import MainPage from "../ui/pages/MainPage"
+
+const routes = [
+    {
+        path:'/',
+        element : <MainPage></MainPage>,
+        loader:() => '메인페이지'
+    }
+ ]
+
+ const router = createBrowserRouter(routes)
+
+ export {router, routes}
